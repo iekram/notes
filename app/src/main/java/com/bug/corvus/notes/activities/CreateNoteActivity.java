678a29957b7591f2013e9c01c86ca76cb3c7fe2d
Ekramul Islam <eislam706@gmail.com>
2021-09-03 21:12:34 +0600

    data store

--- app/src/main/java/com/bug/corvus/notes/activities/CreateNoteActivity.java
+++ app/src/main/java/com/bug/corvus/notes/activities/CreateNoteActivity.java
@@ -1,26 +1,80 @@
 package com.bug.corvus.notes.activities;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-
 import com.bug.corvus.notes.R;
+import com.bug.corvus.notes.database.NotesDatabase;
+import com.bug.corvus.notes.entities.Note;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class CreateNoteActivity extends AppCompatActivity {
+
+    private EditText inputNoteTitle, inputNoteSubtitle, inputNoteText;
+    private TextView textDateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_note);
 
-        ImageView imageBack=findViewById(R.id.image_back);
-        imageBack.setOnClickListener(new View.OnClickListener() {
+        ImageView imageBack = findViewById(R.id.image_back);
+        imageBack.setOnClickListener(v -> onBackPressed());
+
+        inputNoteTitle = findViewById(R.id.input_note_title);
+        inputNoteSubtitle = findViewById(R.id.input_note_subtitle);
+        inputNoteText = findViewById(R.id.input_note);
+        textDateTime = findViewById(R.id.text_date_time);
+        textDateTime.setText(
+                new SimpleDateFormat("EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault())
+                        .format(new Date())
+        );
+        ImageView imageSave = findViewById(R.id.image_save);
+        imageSave.setOnClickListener(v -> saveNote());
+    }
+
+    private void saveNote() {
+        if (inputNoteTitle.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Note title is required", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (inputNoteSubtitle.getText().toString().trim().isEmpty() && inputNoteSubtitle.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Note can't be empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        final Note note = new Note();
+        note.setTitle(inputNoteTitle.getText().toString());
+        note.setSubtitle(inputNoteSubtitle.getText().toString());
+        note.setNoteText(inputNoteText.getText().toString());
+        note.setDateTime(textDateTime.getText().toString());
+
+        @SuppressLint("StaticFieldLeak")
+        class SaveNoteTask extends AsyncTask<Void, Void, Void> {
             @Override
-            public void onClick(View v) {
-                onBackPressed();
+            protected Void doInBackground(Void... voids) {
+                NotesDatabase.getDatabase(getApplicationContext()).noteDao().insert(note);
+                return null;
             }
-        });
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        }
+        new SaveNoteTask().execute();
     }
 }
